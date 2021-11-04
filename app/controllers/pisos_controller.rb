c9a4d80536a66fe8a1d39242a4b8e74ee1064edd
Guilherme Silva <gfsilva@hotmail.com>
2021-11-04 10:53:15 -0300

    rev2

--- app/controllers/pisos_controller.rb
+++ app/controllers/pisos_controller.rb
@@ -17,20 +17,30 @@ class PisosController < ApplicationController
   def create
     @piso = Piso.new(piso_params)
     @piso.save
+    redirect_to pisos_path(@piso)
+  end
+
+
+
+  def edit
+    @piso = Piso.find(params[:id])
+  end
+
+  def destroy
+    @piso = Piso.find(params[:id])
+    @piso.destroy
     redirect_to pisos_path
   end
 
-  private
+
+
+
+private
 
   def piso_params
-    params.require(:pisos).permit(:nome, :tamanho, :valor)
+    params.require(:piso).permit(:nome, :tamanho, :valor, :photo)
   end
 
-# def edit
-#    @piso = Piso.find(params[:id])
-#  end
-def piso_params
-  params.require(:piso).permit(:title, :body, :photo)
-end
+
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,13 @@ Rails.application.routes.draw do
   get 'home', to: 'pisos#home'
   get 'pisos', to: 'pisos#index'
   get 'pisos/new', to: 'pisos#new'
+  post 'pisos/', to: 'pisos#create'
+
   get 'pisos/:id' , to: 'pisos#show', as: :piso
   get "pisos/:id/edit", to: 'pisos#edit', as: :edit_piso
-  post 'pisos/', to: 'pisos#create'
+  patch "pisos/:id", to: "pisos#update"
+  delete "pisos/:id", to: "pisos#destroy"
+
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
