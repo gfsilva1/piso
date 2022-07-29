9f22adc657df29a31894ccf5280cdc74a224af13
Guilherme Silva <gfsilva@hotmail.com>
2022-07-29 17:12:08 -0300

    rev

--- app/models/piso.rb
+++ app/models/piso.rb
@@ -1,6 +1,5 @@
 class Piso < ApplicationRecord
   has_one_attached :photo
-  validates :photo, presence: true
   include PgSearch::Model
   pg_search_scope :busca_piso,
     against: [ :nome ],
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+60.times do
+  teste = Piso.new(nome: "teste")
+  teste.save!
+end
